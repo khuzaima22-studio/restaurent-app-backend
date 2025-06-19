7f31399ed1620005ce560b13c34781746fe9f920
khuzaima22-studio <158497540+khuzaima22-studio@users.noreply.github.com>
2025-06-19 19:02:39 +0500

    Delete src directory

--- src/routes/auth/auth.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { Auth } from "../../controllers/auth/authUser";
-import { SeedUsers } from "../../controllers/users/seedUser";
-
-const router = express.Router();
-
-router.post("/auth", Auth);
-router.get("/seed-user", SeedUsers);
-export default router;
--- src/routes/branch/branch.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { ReadBranches } from "../../controllers/branch/readBranches";
-
-const router = express.Router();
-
-router.get(`/read-branches/:uid`, ReadBranches);
-router.get(`/read-branches`, ReadBranches);
-
-export default router;
